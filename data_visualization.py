d7614d71b6e7ccb03efe2c9850455973976b51fc
ShaneSchroeder <76157271+ShaneSchroeder@users.noreply.github.com>
2022-06-09 19:56:43 -0400

    copilot is really really good

--- data_visualization.py
+++ data_visualization.py
@@ -4,14 +4,18 @@ import pandas as pd
 df = pd.read_csv('iris.data', header=None)
 df.columns = ['Sepal Length', 'Sepal Width', 'Petal Length', 'Petal Width', 'Class']
 
-groupedByClass = df.groupby(df.Class)
+# group the data by class
+df_setosa = df[df['Class'] == 'Iris-setosa']
+df_versicolor = df[df['Class'] == 'Iris-versicolor']
+df_virginica = df[df['Class'] == 'Iris-virginica']
 
-setosa = groupedByClass.get_group("Iris-setosa")
-versicolor = groupedByClass.get_group("Iris-versicolor")
-virginica = groupedByClass.get_group("Iris-virginica")
+# plot df_setosa with blue color and circle shape
+plt.scatter(df_setosa['Sepal Length'], df_setosa['Sepal Width'], color='blue', marker='o')
 
-plt.scatter(setosa["Sepal Length"], setosa["Sepal Width"], color='b', marker='o')
-plt.scatter(versicolor["Sepal Length"], versicolor["Sepal Width"], color='g', marker='*')
-plt.scatter(virginica["Sepal Length"], virginica["Sepal Width"], color='r', marker='p')
+#plot df_versicolor with green color and star shape
+plt.scatter(df_versicolor['Sepal Length'], df_versicolor['Sepal Width'], color='green', marker='*')
+
+#plot df_virginica with red color and square shape
+plt.scatter(df_virginica['Sepal Length'], df_virginica['Sepal Width'], color='red', marker='s')
 
 plt.show()
